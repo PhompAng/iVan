1bbadb46773cd3db21f4bea3599dad522ad16833
PhompAng <tingtong003tomy@gmail.com>
2017-08-08 10:14:15 +0700

    login form

---
+++ ivan-admin/src/components/auth/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login row justify-content-md-center">
+    <div class="col-md-8">
+      <div class="card">
+        <h3 class="card-header">Login</h3>
+        <div class="card-block">
+          <form action="#">
+            <div class="form-group row">
+              <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+              <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+              <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+              </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
