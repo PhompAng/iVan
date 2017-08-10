dc65d7c7f42c48d5f9c70f994e4aa0f2519689e3
PhompAng <tingtong003tomy@gmail.com>
2017-08-10 15:08:58 +0700

    firebase setup

--- ivan-admin-backend/src/index.js
+++ ivan-admin-backend/src/index.js
@@ -1,4 +1,11 @@
 import server from '~/server'
+import * as admin from 'firebase-admin'
+var serviceAccount = require('~/resources/ivan-f1006-firebase-adminsdk-qvw3d-7f360dc20e.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://ivan-f1006.firebaseio.com"
+});
 
 server.listen(3000, function () {
     console.log('Example app listening on port 3000!')
--- ivan-admin-backend/src/server/root.js
+++ ivan-admin-backend/src/server/root.js
@@ -1,5 +1,22 @@
+import * as admin from 'firebase-admin'
+
 export default (app) => {
     app.get('/', function (req, res) {
         res.send('Hello World!')
+    })
+    app.get('/gen', async function (req, res) {
+        try {
+            var userRecord = await admin.auth().createUser({
+                email: "user@example.com",
+                emailVerified: false,
+                password: "123456",
+                displayName: "John Doe",
+                disabled: false
+            })
+            res.send(userRecord.uid)
+        } catch (err) {
+            console.log("Error creating new user:", err)
+            res.send(err)
+        }
     })
 }
